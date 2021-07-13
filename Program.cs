eabd5604cb3450107c8fb57a97998767f1920001
Tanisa124 <41618274+Tanisa124@users.noreply.github.com>
2021-07-13 13:16:16 +0700

    Add files via upload
    Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_
+{
+    ///first console app lecture code
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
